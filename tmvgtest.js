a2742187905c1299f7ff078e5324741a7d02a523
VincentG2021 <vincentgade@gmail.com>
2021-05-28 11:40:26 +0200

    update proto task manager basic features

--- tmvgtest.js
+++ tmvgtest.js
@@ -1,9 +1,27 @@
 //TASK MANAGER VG2021//
 
 //declare GLOBAL VARIABLES//
-let tasks = ["task1", "task2", "task3"];
+let tasksList = [];
+let taskDoneList = [];
 
 //define FUNCTIONS//
+
+//read into JSON files//
+const readjson = require('fs');
+const taskslistFile = readjson.readFileSync('taskslisttodo.json', 'utf-8');
+const tasksdoneFile = readjson.readFileSync('taskslistdone.json', 'utf-8');
+
+// //save data into JSON files//
+// let tasksList = JSON.parse(taskFile);
+// let finishtask = JSON.parse(doneFile);
+// const saveData = (json, man) => {
+//     let data = JSON.stringify(man);
+//     fs.writeFile( json, data, finished);
+//     function finished(err) {
+//         return err;
+//     }
+// }
+
 //get user INPUT//
 const readline = require("readline");
 const rl = readline.createInterface({
@@ -21,35 +39,37 @@ console.log("2. to add a task");
 console.log("3. to delete a task");
 console.log("4. to mark a task as done");
 console.log("5. to Exit the task manager");
-};
+
 
 //get the user CHOICE & program reaction//
-tmmenu();
+// tmmenu();
 
 	rl.question("What is your choice ? ", (choice) =>{
 	if (choice == 1){
-		console.log(tasks);
+		console.log(tasksList);
+
 		tmmenu();
 	}
 	
 	else if (choice == 2){
 		rl.question("type the task to add :", (taskadd) =>{
-			tasks.push(taskadd);
-			console.log("task added");
+			tasksList.push(taskadd);
+			console.log(`${taskadd} added`);
 			tmmenu();
 	})
 
 	}else if(choice == 3){
 		rl.question("type the index of the task to delete :", (taskdel) =>{
-			tasks.splice(taskdel);
-			console.log("task deleted");
+			tasksList.splice(tasksList.indexOf(taskdel), 1);
+			console.log(`${taskdel} deleted`);
 			tmmenu();
 		})
 
 	}else if (choice == 4){
-		rl.question("type the task to mark :", (taskdone) =>{
-			tasks.done(taskdone);
-			console.log("task marked as done");
+		rl.question("type the task to mark as done:", (taskdone) =>{
+			tasksList.splice(tasksList.indexOf(taskdone), 1);
+			taskDoneList.push(taskdone);
+			console.log(`${taskdone} marked as done`);
 			tmmenu();
 		})
 		
@@ -59,6 +79,8 @@ tmmenu();
 		}
 }
 	);
+};
+tmmenu();
 	
 
 
